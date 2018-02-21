95145134e3ef157d4a3b03afbec64dcb54417aed
SHUNPAN LO <shunpanlo@SHUNPANde-MacBook-Air.local>
2018-02-21 21:08:59 +0800

    first commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by SHUNPAN LO on 21/2/2018.
+//  Copyright © 2018年 bensonlspapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
